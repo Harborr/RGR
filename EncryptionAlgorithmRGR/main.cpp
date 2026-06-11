5442dce4d4c53f2920556603e732a4f6e406fb68
gojo-satoru-is-alive <148470412+Alen-ai@users.noreply.github.com>
2026-06-11 21:53:19 +0700

    Create main.cpp

---
+++ EncryptionAlgorithmRGR/main.cpp
@@ -0,0 +1,18 @@
+#include "UI/Menu.h"
+#include <iostream>
+#include <locale>
+
+int main() {
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+    cout << "\n  ______                     _   _             " << endl;
+    cout << " |  ____|                   | | (_)            " << endl;
+    cout << " | |__   _ __   ___ _ __ ___| |_ _  ___  _ __  " << endl;
+    cout << " |  __| | '_ \\ / __| '__/ __| __| |/ _ \\| '_ \\ " << endl;
+    cout << " | |____| | | | (__| | | (__| |_| | (_) | | | |" << endl;
+    cout << " |______|_| |_|\\___|_|  \\___|\\__|_|\\___/|_| |_|" << endl;
+    cout << endl;
+    cout << "  Шифровальщик РГР v1.0" << endl;
+    Menu m;
+    m.show();
+    return 0;
+}
