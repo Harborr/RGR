cb15373227dac0af2e7781eddf652577a347e388
Verkhorurov Vladimir <haubor@bk.ru>
2026-06-13 00:00:37 +0700

    delete

--- vigenere.h
+++
@@ -1,6 +0,0 @@
-#ifndef VIGENERE_H
-#define VIGENERE_H
-#include <string>
-std::string vigenereEncrypt(const std::string& text, const std::string& key);
-std::string vigenereDecrypt(const std::string& text, const std::string& key);
-#endif
